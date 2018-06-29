75329d14b9d4ddbc90aae5bf40731a93a655238a
Areeta Wong <areetaw@gmail.com>
2018-06-29 19:56:19 +0000

    Changed CSS for all + included functions on A-Frame

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,7 @@
-function myFunction() {
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
 
-        }
-    }
+document.onload = function () {
+    console.log("does this work");
+    document.getElementById("enter-aframe").addEventListener("click", function() {
+        window.open("aframe.html");
+    });
 }
